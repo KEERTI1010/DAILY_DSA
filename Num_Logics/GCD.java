de31bcc55d49ffbfe0a51284a8fb717528338956
KEERTI1010 <shetkeerti6@gmail.com>
2026-06-03 23:52:54 +0530

    Daily_DSA

--- Num_Logics/GCD.java
+++ Num_Logics/GCD.java
@@ -20,3 +20,30 @@ public class GCD
 	    System.out.println(gcd);
 	}
 }
+
+
+
+/*
+
+DSA Method
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD = " + a);
+    }
+} */
