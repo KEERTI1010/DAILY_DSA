64e3e7644263a5dc9dce8e062919dcce98cdc152
KEERTI1010 <shetkeerti6@gmail.com>
2026-06-11 18:43:22 +0530

    Daily_DSA

--- Num_Logics/XPowerNByFactorial.java
+++ Num_Logics/XPowerNByFactorial.java
@@ -1,3 +1,32 @@
+// Write a program to compute x^n/n!.........
+
+import java.util.Scanner;
+
+public class XPowerNByFactorial
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int factorial=1;
+		int power = 1;
+
+		int x = sc.nextInt();
+		
+		for(int i=1 ; i<=n ; i++){
+		    factorial*=i;
+			power = power * x;
+		}
+		
+		System.out.println(power/factorial);
+	}
+}
+
+
+/*
+
+with Math pow
+
 import java.util.Scanner;
 
 public class XPowerNByFactorial
@@ -16,3 +45,4 @@ public class XPowerNByFactorial
 		System.out.println(pow/factorial);
 	}
 }
+*/
