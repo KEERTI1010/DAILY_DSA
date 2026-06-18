e920b3208e1a72736eaeb0ef4dc5f5041c4beac4
KEERTI1010 <shetkeerti6@gmail.com>
2026-06-18 07:28:33 +0530

    Daily_DSA

--- Num_Logics/Main.java
+++ Num_Logics/Main.java
@@ -2,7 +2,38 @@
 //  A Magic Number is a number whose repeated sum of digits ultimately becomes 1. For example, 1729 is a Magic Number because 1+7+2+9=19, 1+9=10, and 1+0=1
 
 import java.util.Scanner;
-public class Ma
+public class Main
+{
+	public static boolean isMagical(int n){
+	    do{
+	        int sum = 0;
+	        while(n>0){
+	           sum = sum + (n%10);
+	           n = n / 10 ;
+	        }
+	        n = sum ;
+	    }while(n >= 10);
+	   
+	    return n==1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		
+		if (isMagical(n)){
+		    System.out.println("Its an Magical Num");
+		}
+		else{
+		    System.out.println("Its not an Magical Num");
+		}
+	}
+}
+
+
+/*
+import java.util.Scanner;
+public class Magical
 {
 	public static boolean isMagical(int n){
 	    while(n>=10){
@@ -29,3 +60,4 @@ public class Ma
 		}
 	}
 }
+*/
