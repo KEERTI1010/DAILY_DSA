07cddf003d71b0a7f3c9592c77ebd9500ea61dd5
KEERTI1010 <shetkeerti6@gmail.com>
2026-07-26 18:28:03 +0530

    Daily DSA

---
+++ Num_Logics/EvilSeries.java
@@ -0,0 +1,29 @@
+// Write a Program to Find out all Evil numbers present within a given range.
+
+
+import java.util.Scanner;
+public class EvilSeries
+{
+	public static boolean isEvilSeries(int n){
+	    int count = 0;
+	    while(n>0){
+	        if(n % 2 == 1){
+	            count ++ ;
+	        }
+	        n = n / 2;
+	    }
+	    return count % 2 == 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		for(int k=a ; k<=b ; k++){
+		    if(isEvilSeries()){
+		        System.out.println(k);
+		    }
+		}
+	}
+}
